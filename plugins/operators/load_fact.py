242b4e5484d05edbf50e505a56da8b708697e3e5
sahilchutani <sc4617@columbia.edu>
2021-01-14 13:14:50 -0500

    added schema to insert fact table statement

--- plugins/operators/load_fact.py
+++ plugins/operators/load_fact.py
@@ -9,18 +9,20 @@ class LoadFactOperator(BaseOperator):
     @apply_defaults
     def __init__(self,
                  redshift_conn_id='',
+                 schema='',
                  table='',
                  sql_stmt='',
                  *args, **kwargs):
 
         super(LoadFactOperator, self).__init__(*args, **kwargs)
         self.redshift_conn_id = redshift_conn_id
+        self.schema = schema
         self.table = table
         self.sql_stmt = sql_stmt
 
     def execute(self, context):
         self.log.info("Loading songplays table")
         redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
-        formatted_sql = f"INSERT INTO {self.table} ({self.sql_stmt})"
+        formatted_sql = f"INSERT INTO {self.schema}.{self.table} ({self.sql_stmt})"
         redshift.run(formatted_sql
         self.log.info("Success: loaded songplays table")
